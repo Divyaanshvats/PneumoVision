ee4e63c6d4232ae5f08dea30178990569a53f9f2
Divyaansh Vats <122478574+Divyaanshvats@users.noreply.github.com>
2026-08-29 11:43:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+from PIL import Image
+
+# load DenseNet121
+# load densenet121_pneumonia.pth
+# upload image
+# preprocess
+# predict
+# display result
